5281169dcc27b053661ab3c9cc2d1cbcdd273b0e
andrejaccedo <89917860+andrejaccedo@users.noreply.github.com>
2022-04-27 23:55:28 +0200

    wait for 20 seconds

--- src/workflow.js
+++ src/workflow.js
@@ -1,50 +1,48 @@
 const axiosDefault = require("axios");
 const _ = require("underscore");
 
-_.mixin({
-  CSVtoJSON: (strData, strDelimiter) => {
-    strDelimiter = strDelimiter || ",";
-    if (strData.substr(-1) !== "\n") strData += "\n";
+const CSVtoJSON = (strData, strDelimiter) => {
+  strDelimiter = strDelimiter || ",";
+  if (strData.substr(-1) !== "\n") strData += "\n";
 
-    let objPattern = new RegExp(
-      "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" + '(?:"([^"]*(?:""[^"]*)*)"|' + '([^"\\' + strDelimiter + "\\r\\n]*))",
-      "gi"
-    );
+  let objPattern = new RegExp(
+    "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" + '(?:"([^"]*(?:""[^"]*)*)"|' + '([^"\\' + strDelimiter + "\\r\\n]*))",
+    "gi"
+  );
 
-    const jsonData = [];
-    let arrMatches = null;
-    let keys = [];
-    let row = [];
+  const jsonData = [];
+  let arrMatches = null;
+  let keys = [];
+  let row = [];
 
-    while ((arrMatches = objPattern.exec(strData))) {
-      const strMatchedDelimiter = arrMatches[1];
+  while ((arrMatches = objPattern.exec(strData))) {
+    const strMatchedDelimiter = arrMatches[1];
 
-      const newRow = strMatchedDelimiter.length && strMatchedDelimiter !== strDelimiter;
+    const newRow = strMatchedDelimiter.length && strMatchedDelimiter !== strDelimiter;
 
-      if (newRow) {
-        if (!keys.length) {
-          keys = row;
-        } else {
-          let data = {};
-          for (let i in row) {
-            data[keys[i]] = row[i];
-          }
-          jsonData.push(data);
-        }
-        row = [];
-      }
-
-      let strMatchedValue;
-      if (arrMatches[2]) {
-        strMatchedValue = arrMatches[2].replace(new RegExp('""', "g"), '"');
+    if (newRow) {
+      if (!keys.length) {
+        keys = row;
       } else {
-        strMatchedValue = arrMatches[3];
+        let data = {};
+        for (let i in row) {
+          data[keys[i]] = row[i];
+        }
+        jsonData.push(data);
       }
-      row.push(strMatchedValue);
+      row = [];
     }
-    return jsonData;
-  },
-});
+
+    let strMatchedValue;
+    if (arrMatches[2]) {
+      strMatchedValue = arrMatches[2].replace(new RegExp('""', "g"), '"');
+    } else {
+      strMatchedValue = arrMatches[3];
+    }
+    row.push(strMatchedValue);
+  }
+  return jsonData;
+};
 
 exports.main = async (event, callback) => {
   try {
@@ -59,11 +57,23 @@ exports.main = async (event, callback) => {
       },
     });
 
-    const { data: file } = await axiosDefault.get("http://localhost:8080/uploads/data.csv", { responseType: "blob" });
-    const tasks = await _(file).CSVtoJSON();
+    const { data: file } = await axiosDefault.get(
+      "https://gist.githubusercontent.com/NickDeckerDevs/92f673c76212d7034c52c9353ccf94ea/raw/94519de9a4a4c605282c4ec96ddb926f72876569/alltheseitesmascsv.csv",
+      { responseType: "blob" }
+    );
+    const tasks = CSVtoJSON(file);
 
-    tasks.forEach(async (task) => {
+    let interation = 0;
+    for await (const task of tasks) {
       try {
+        interation++;
+        console.log(interation);
+
+        if (interation % 20 === 0) {
+          // Every 20 interation for 5 seconds
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+        }
+
         const properties = {
           hs_timestamp: task.due_date * 1000,
           hs_task_body: task.details,
@@ -81,7 +91,7 @@ exports.main = async (event, callback) => {
       } catch (err) {
         console.log(err);
       }
-    });
+    }
 
     callback({
       outputFields: {
